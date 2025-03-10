6a6aecc3a5cf0f5dd8caa86d467d06b48bb9f45b
Ruslan <sounditbox@gmail.com>
2025-03-10 21:43:42 +0400

    Refactored app.py

--- app.py
+++ app.py
@@ -13,41 +13,51 @@ MAX_FILE_SIZE = 5 * 1024 * 1024
 class ImageHostingHttpRequestHandler(BaseHTTPRequestHandler):
     server_version = 'Image Hosting Server v0.1'
 
+    def __init__(self, request, client_address, server):
+        self.get_routes = {
+            '/api/images': self.get_images,
+            '/upload/': self.get_upload
+        }
+        self.post_routes = {
+            '/upload/': self.post_upload
+        }
+        self.default = lambda: self.send_html('404.html', 404)
+        super().__init__(request, client_address, server)
+
     def do_GET(self):
-        if self.path == '/api/images':
-            self.send_response(200)
-            self.send_header('Content-type', 'application/json')
-            self.end_headers()
-            response = {
-                'images': next(os.walk(IMAGES_PATH))[2]
-            }
-            self.wfile.write(json.dumps(response).encode('utf-8'))
-        elif self.path == '/images/':
-            self.send_html('images.html')
-        elif self.path == '/upload/':
-            self.send_html('upload.html')
-        else:
-            self.send_html('404.html', 404)
+        self.get_routes.get(self.path, self.default)()
+
+    def get_images(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        response = {
+            'images': next(os.walk(IMAGES_PATH))[2]
+        }
+        self.wfile.write(json.dumps(response).encode('utf-8'))
+
+    def get_upload(self):
+        self.send_html('upload.html')
 
     def do_POST(self):
-        if self.path == '/upload/':
-            length = int(self.headers.get('Content-Length'))
-            if length > MAX_FILE_SIZE:
-                self.send_html('upload_failed.html', 413)
-                return
+        self.post_routes.get(self.path, self.default)()
 
-            data = self.rfile.read(length)
-            _, ext = os.path.splitext(self.headers.get('Filename'))
-            image_id = uuid4()
-            if ext not in ALLOWED_EXTENSIONS:
-                self.send_html('upload_failed.html', 400)
-                return
+    def post_upload(self):
+        length = int(self.headers.get('Content-Length'))
+        if length > MAX_FILE_SIZE:
+            self.send_html('upload_failed.html', 413)
+            return
 
-            with open(IMAGES_PATH + f'{image_id}{ext}', 'wb') as file:
-                file.write(data)
-            self.send_html('upload_success.html')
-        else:
-            self.send_html('404.html', 404)
+        data = self.rfile.read(length)
+        _, ext = os.path.splitext(self.headers.get('Filename'))
+        image_id = uuid4()
+        if ext not in ALLOWED_EXTENSIONS:
+            self.send_html('upload_failed.html', 400)
+            return
+
+        with open(IMAGES_PATH + f'{image_id}{ext}', 'wb') as file:
+            file.write(data)
+        self.send_html('upload_success.html')
 
     def send_html(self, file_path, code=200):
         self.send_response(code)
