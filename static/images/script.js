83181e0ef8cbfef59a4a0ecd34ae0fd0331e58d5
Ruslan <sounditbox@gmail.com>
2025-03-24 20:08:52 +0400

    Fixed redundant slashes

--- static/images/script.js
+++ static/images/script.js
@@ -25,11 +25,11 @@ function setImages(images) {
         }
         deleteButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16"> <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/></svg>';
         tdDelete.appendChild(deleteButton);
-        tdPreview.innerHTML = `<img src="/images/${image}/" width="42" height="100%">`;
+        tdPreview.innerHTML = `<img src="/images/${image}" width="42" height="100%">`;
         deleteButton.classList.add('delete-btn');
         tdDelete.appendChild(deleteButton);
-        tdPreview.innerHTML = `<img src="/images/${image}/" width="42" height="100%">`;
-        tdUrl.innerHTML = `<a href="/images/${image}/" target="_blank">${image}</a>`;
+        tdPreview.innerHTML = `<img src="/images/${image}" width="42" height="100%">`;
+        tdUrl.innerHTML = `<a href="/images/${image}" target="_blank">${image}</a>`;
 
         tr.appendChild(tdPreview);
         tr.appendChild(tdUrl);
